f55bd9f95dde48c3861f61b902015da4d168fc3e
Yazooliu <39296687+Yazooliu@users.noreply.github.com>
2019-07-16 23:35:33 +0800

    updated

--- string/stringall.cpp
+++ string/stringall.cpp
@@ -3,74 +3,46 @@
 #include<vector>
 #include<set>
 using namespace std;
-vector<string> split(const string &str, const string &pattern)
+
+vector<string> split(string str, vector<string> ListOfToken)
 {
     vector<string> res;
-    if(str == "")
-        return res;
-    //在字符串末尾也加入分隔符，方便截取最后一段
-    string strs = str + pattern;
-    size_t pos = strs.find(pattern);
-
-    while(pos != strs.npos)
-    {
-        string temp = strs.substr(0, pos);
-        res.push_back(temp);
-        //去掉已分割的字符串,在剩下的字符串中进行分割
-        strs = strs.substr(pos+1, strs.size());
-        pos = strs.find(pattern);
-    }
-
+    // if input or ListofToken is empty //
+	if(str.size() == 0)
+		return res;
+	if(ListOfToken.size()== 0){
+		str = str.substr(0,str.size());
+		res.push_back(str);
+	}
+    // set ch as set as no depuliacted fromListOfToken//
+	set<char> ch;
+	for(int i=0;i<ListOfToken.size();i++)
+		for(int j=0;j<ListOfToken[i].size();j++)
+			ch.insert(ListOfToken[i][j]);
+	
+    for(set<char>::iterator iter = ch.begin();iter!=ch.end();iter++){
+		size_t pos = str.find(*iter);
+		//ch.erase(iter);
+		if(pos!= str.npos){
+			string curstring = str.substr(0,pos); // pos = length 
+			res.push_back(curstring);
+			str = str.substr(pos+1,str.size());
+		}
+	}
+	res.push_back(str);
     return res;
 }
-
-class Solution{
-public:
-	vector<string> Split(string input,vector<string>ListofToken,vector<string> &res){
-		string tmpstr;
-	
-		
-		// if input or ListofToken is empty //
-		if(input.size() == 0)
-			return res;
-	    if(ListofToken.size()== 0){
-			tmpstr = input.substr(0,input.size());
-			res.push_back(tmpstr);
-		}
-
-		// if input and ListofToken are not empty //
-		set<char> records;
-		for(int i = 0;i<ListofToken.size();i++)
-			for(int j=0;j<ListofToken[i].size();j++)
-				records.insert(ListofToken[i][j]);
-			
-		//
-		int rawinputsize = input.size();
-		for(int i = 0;i<input.size();i++)
-			if(records.find(input[i])!= records.end()) // in input, can find char in ListofToken
-			{
-				tmpstr = input.substr(0,i);
-				res.push_back(tmpstr);
-				input = input.substr(i+1,rawinputsize-i-1);
-				//i = 0;
-			}
-			
-				
-	}
-};
-
 
 void main()
 {
   string inputs = "abc,def.ghi";
-  size_t pos1st = inputs.find("ef");
-  string c[]=  {"ef","c"};
+  vector<string> strs;
+  strs.push_back("ef");
+  strs.push_back("v");
+  vector<string> res = split(inputs,strs);
   
-  vector<string>ListofToken;
-  ListofToken.push_back(c[0]);
-  ListofToken.push_back(c[1]);
-  vector<string> res;
-  Solution().Split(inputs,ListofToken,res);
+  for(int i = 0;i<res.size();i++)
+	    cout<<res[i]<<" ";
   
   getchar();
 }
